f998bbc0dea2732bdb80825d2122c3e95ad1b771
Xavier <xaviervelasquez2002@gmail.com>
2023-02-17 15:26:27 -0800

    Finished

---
+++ Develop/README.md
@@ -0,0 +1,39 @@
+# words
+
+  [![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)
+
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Licenses](#licenses)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [Credits](#credits)
+
+  ## Description
+  words
+
+  ## Installation
+  words
+
+  ## Usage
+  words
+
+  ## Licenses
+    This project is covered under the MIT license. To learn more about what this means, click the license button at the top.
+
+  ## Contributing
+  words
+
+  ## Tests
+  words
+
+  ## Questions
+  Feel free to ask away 
+  GitHub: https://github.com/words  
+  Email: words
+
+  ## Credits
+  words
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,172 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Please provide your full name:',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("You should probably provide a name, don't you think?");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Agin, you should probably provide a username.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please provide your contact information for your project.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Why no name for your project?');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Enter your project description here:",
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('How am I supposed to know what your project is?');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the instructions for installation?',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('Please provide instructions for installations.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Instructions for usage:',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Providing instructions for usage.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can others contribute to this project?',
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('Please provide instructions on how others can contribute.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Describe the tests written for your application and how to use them:',
+        validate: testsInput => {
+            if (testsInput) {
+                return true;
+            } else {
+                console.log('Please provide an overview of your tests.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmLicenses',
+        message: 'Would you like to include a license?',
+        default: false
+    },
+    {
+        type: 'list',
+        name: 'licenses',
+        message: 'What license would you like to include?',
+        choices: ['MIT', 'GPL', 'CC--0'],
+        when: ({ confirmLicenses }) => {
+            if (confirmLicenses) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = data => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./README.md', data, err => {
+            if (err) {
+                reject (err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: console.log('All done. Go ahead and check out your new README.md.')
+            });
+        })
+    })
+};
 
-// TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions);
+};
 
-// Function call to initialize app
-init();
+init()
+.then(userInput => {
+    return generateMarkdown(userInput);
+})
+.then(readmeInfo => {
+    return writeToFile(readmeInfo);
+})
+.catch(err => {
+    console.log(err);
+});
